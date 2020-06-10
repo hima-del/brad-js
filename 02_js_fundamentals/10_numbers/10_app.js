f83d0e0a0a3c378d23fa82c56088bf517fbb7cae
hima-del <himajaharidas77@gmail.com>
2020-06-10 23:53:02 +0530

    numbers

---
+++ 02_js_fundamentals/10_numbers/10_app.js
@@ -0,0 +1,26 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+//math object
+val = Math.PI;
+val = Math.E;
+val = Math.round(3.5);
+val = Math.ceil(6.9);
+val = Math.floor(6.9);
+val = Math.sqrt(67);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2, 33, 23, 45, -1, 0);
+val = Math.max(8, 67, 55, -9, 0);
+val = Math.floor(Math.random() * 20);
+
+
+
+console.log(val);
