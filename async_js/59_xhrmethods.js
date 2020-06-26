baafda774f14784147bb16a02530a2a294b4cf85
hima-del <himajaharidas77@gmail.com>
2020-06-26 15:47:02 +0530

    async js

---
+++ async_js/59_xhrmethods.js
@@ -0,0 +1,44 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+function loadData() {
+    //cretae xhr object
+    const xhr = new XMLHttpRequest();
+    //OPEN
+    xhr.open('GET', 'data.txt', true);
+    // console.log('READYSTATE', xhr.readyState);
+    //optional -used for spinners / loaders
+    xhr.onprogress = function() {
+        console.log('READYSTATE', xhr.readyState);
+    }
+
+
+    xhr.onload = function() {
+            console.log('READYSTATE', xhr.readyState);
+            if (this.status === 200) {
+                // console.log(this.responseText);
+                document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`
+            }
+        }
+        // xhr.onreadystatechange = function() {
+        //     console.log('READYSTATE', xhr.readyState);
+        //     if (this.status === 200 && this.readyState === 4) {
+        //         console.log(this.responseText);
+        //     }
+        // }
+    xhr.onerror = function() {
+        console.log('Request erroe...');
+    }
+    xhr.send();
+
+
+    //redy state values
+    //0:request not initialized
+    //1:serever connection established
+    //2:request recieved
+    //3:processing request
+    //4:request finished
+    //http statuses
+    //200: ok
+    //403: forbidden
+    //404: not found
+}
