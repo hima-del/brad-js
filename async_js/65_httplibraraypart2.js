8b22a8df12ee604ca00101fae74521a03fc5b723
hima-del <himajaharidas77@gmail.com>
2020-06-27 00:35:19 +0530

    http library part2

--- async_js/65_httplibraraypart2.js
+++ async_js/65_httplibraraypart2.js
@@ -1,19 +1,23 @@
 const http = new easyHTTP;
 //get posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(posts);
-    }
-    // console.log(posts);
-});
-//get single post
-http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(post);
-    }
-    // console.log(posts);
-});
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+//     // console.log(posts);
+// });
+// //get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+//     // console.log(posts);
+// });
+//create data
+const data = {
+
+}
