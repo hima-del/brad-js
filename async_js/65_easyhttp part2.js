43340f2e1395a2d0e084ed3022aa999c3118daa9
hima-del <himajaharidas77@gmail.com>
2020-06-27 09:51:58 +0530

    http library part2

--- async_js/65_easyhttp part2.js
+++ async_js/65_easyhttp part2.js
@@ -17,13 +17,39 @@ easyHTTP.prototype.get = function(url, callback) {
     }
     //make an http post request
 easyHTTP.prototype.post = function(url, data, callback) {
-    this.http.open('POST', url, true);
-    this.http.setRequestHeader('Content-type', 'application/json');
+        this.http.open('POST', url, true);
+        this.http.setRequestHeader('Content-type', 'application/json');
+        let self = this;
+        this.http.onload = function() {
+            callback(null, self.http.responseText);
+
+        }
+
+        this.http.send(JSON.stringify(data));
+    }
+    //make an http put request
+easyHTTP.prototype.put = function(url, data, callback) {
+        this.http.open('PUT', url, true);
+        this.http.setRequestHeader('Content-type', 'application/json');
+        let self = this;
+        this.http.onload = function() {
+            callback(null, self.http.responseText);
+
+        }
+
+        this.http.send(JSON.stringify(data));
+    }
+    //make an http delete request
+easyHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+
     let self = this;
     this.http.onload = function() {
-        callback(null, self.http.responseText);
-
+        if (self.http.status === 200) {
+            callback(null, 'Post Deleted');
+        } else {
+            callback('error:' + self.http.status);
+        }
     }
-
-    this.send(JSON.stringify(data));
+    this.http.send();
 }
