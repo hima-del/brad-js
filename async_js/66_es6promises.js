5ad59ad61e4b4be1e88e7b49dd31b9728a5cfede
hima-del <himajaharidas77@gmail.com>
2020-06-27 11:32:32 +0530

    es6 promise

---
+++ async_js/66_es6promises.js
@@ -0,0 +1,51 @@
+const posts = [
+    { title: 'post one', body: 'this is post one' },
+    { title: 'post two', body: 'this is post two' },
+];
+
+// function createPost(post) {
+//     setTimeout(function() {
+//         posts.push(post);
+//     }, 2000);
+// }
+
+// function getPosts() {
+//     setTimeout(function() {
+//         let output = '';
+//         posts.forEach(function(post) {
+//             output += `<li>${post.title}</li>`
+//         });
+//         document.body.innerHTML = output;
+//     }, 1000);
+// }
+// createPost({ title: 'post three', body: 'this is post three' });
+// getPosts();
+function createPost(post) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            posts.push(post);
+            const error = true;
+            if (!error) {
+                resolve();
+            } else {
+                reject('error! something went wrong');
+            }
+        }, 2000);
+    });
+}
+
+function getPosts() {
+    setTimeout(function() {
+        let output = '';
+        posts.forEach(function(post) {
+            output += `<li>${post.title}</li>`
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+createPost({ title: 'post three', body: 'this is post three' })
+    .then(getPosts)
+    .catch(function(err) {
+        console.log(err);
+    });
+// getPosts();
