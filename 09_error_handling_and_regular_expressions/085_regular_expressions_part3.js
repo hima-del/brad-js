056ded2e10f769bdc94229460394210206002bb5
hima-del <himajaharidas77@gmail.com>
2020-06-30 20:44:13 +0530

    regular expressions part3

---
+++ 09_error_handling_and_regular_expressions/085_regular_expressions_part3.js
@@ -0,0 +1,45 @@
+let re;
+//literal characters
+re = /hello/;
+re = /hello/i;
+//metacharacter symbols
+re = /^h/i; // must start with h
+re = /d$/i; //must ends with d
+re = /^hello$/; //must start with hello and end eith hello
+re = /^h.llo$/; //. matches any one charecter
+re = /h*llo/; //matches a charcter 0 or more times
+re = /gra?ay/i; //optional character e and a
+re = /gra?a\?y/i; //escape character e and a
+//brackets[]-character sets
+re = /gr[ae]y/i; // must be an a or e
+re = /[GF]ray/i; // must be an G or F
+re = /[^ae]ray/i; // match anything except a or e
+re = /[A-Z]ray/; // MATCH ANY UPPERCASE LETTER
+re = /[a-z]ray/i; // match nay lower case letter
+re = /[A-Za-z]ray/; // match any letter
+re = /[0-9]ray/; //match nay digit
+re = /[0-9][0-9]ray/; //match nay digit
+//braces{}-quantifiers
+re = /hel{2}o/; //must occur exactlt {m} amount of times
+re = /he{2,4}o/; //occur between 2 and 4
+re = /he{2,}o/; //must occur atleast {m} times
+//paranthesis()-grouping
+re = /([0-9]x){3}/;
+
+const str = 'hello';
+//const str = 'grey';
+//const str = '10ray';
+//string to match
+//const str = 'Hello World';
+//log results
+const result = re.exec(str);
+console.log(result);
+
+function reTest(re, str) {
+    if (re.test(str)) {
+        console.log(`${str} matches ${re.source}`);
+    } else {
+        console.log(`${str} does not match ${re.source}`);
+    }
+}
+reTest(re, str);
