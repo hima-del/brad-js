f42df65e2ff4f775d32e8620f48bbf188d59fd7d
hima-del <himajaharidas77@gmail.com>
2020-08-01 00:09:22 +0530

    chapter4 completed

--- headfirst_javascript/chapter4.js
+++ headfirst_javascript/chapter4.js
@@ -49,12 +49,57 @@ let scores2 = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
     34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
     46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44
 ];
-let output2;
-for (let l = 0; l < scores2.length; l++) {
-    output2 = "bubble solution #" + l + " score: " + scores2[l];
-    console.log(output2);
-}
+let costs = [.25, .27, .25, .25, .25, .25,
+    .33, .31, .25, .29, .27, .22,
+    .31, .25, .25, .33, .21, .25,
+    .25, .25, .28, .25, .24, .22,
+    .20, .25, .30, .25, .24, .25,
+    .25, .25, .27, .25, .26, .29
+];
 
+function printHighScore(scores2) {
+    let highScore = 0;
+    let output2;
+    for (let l = 0; l < scores2.length; l++) {
+        output2 = "bubble solution #" + l + " score: " + scores2[l];
+        console.log(output2);
+        if (scores2[l] > highScore) {
+            highScore = scores2[l];
+        }
+    }
+    return highScore;
+}
+let highScore = printHighScore(scores2);
+console.log("total bubble tests " + scores2.length);
+console.log("bubble with highest score " + highScore);
+
+function getBestResults(scores2, highScore) {
+    let bestSolutions = [];
+    for (let m = 0; m < scores2.length; m++) {
+        if (scores2[m] == highScore) {
+            bestSolutions.push(m);
+        }
+    }
+    return bestSolutions;
+}
+let bestSolutions = getBestResults(scores2, highScore);
+console.log("solutions with highest score: " + bestSolutions);
+
+function getMostCostEffectiveSolution(scores2, costs, highScore) {
+    let cost = 100;
+    let index;
+    for (let n = 0; n < scores2.length; n++) {
+        if (scores2[n] == highScore) {
+            if (cost > costs[n]) {
+                index = n;
+                cost = costs[n];
+            }
+        }
+    }
+    return index;
+}
+let mostCostEffective = getMostCostEffectiveSolution(scores2, costs, highScore);
+console.log("bubble solution #" + mostCostEffective + " is the most cost effective solution");
 //code magnet using while loop
 let products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
 let hasBubbleGum = [false, false, false, true];
