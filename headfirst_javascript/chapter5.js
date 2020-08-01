f318d3255d99a4469b84241db38578575e351b70
hima-del <himajaharidas77@gmail.com>
2020-08-01 12:28:39 +0530

    adding objects to functions

--- headfirst_javascript/chapter5.js
+++ headfirst_javascript/chapter5.js
@@ -11,8 +11,8 @@ let chevy = {
 
 //another object-exercise
 let cadi = {
-    make: "cadi",
-    model: "GM Cadillac",
+    make: "GM",
+    model: "Cadillac",
     year: 1955,
     color: "tan",
     convertible: false,
@@ -31,21 +31,82 @@ let fiat = {
     mileage: 88000
 };
 
-let miles = fiat.mileage;
-if (miles < 2000) {
-    buyIt();
+// let miles = fiat.mileage;
+// if (miles < 2000) {
+//     buyIt();
+// }
+
+// //change a aproperty
+// fiat.mileage = 10000;
+
+// //how to add a new property
+// fiat.needsWashing = true;
+
+// //how to compute with properties
+// if (fiat.year > 1965) {
+//     classic = true;
+// }
+// for (let i = 0; i < fiat.passengers; i++) {
+//     addPersonToCar();
+// }
+
+//code magnet
+let dog = {
+    name: "Fido",
+    weight: 20.2,
+    age: 4,
+    breed: "mixed",
+    activity: "fetch balls"
+};
+let bark;
+if (dog.weight > 20) {
+    bark = "WOOF WOOF";
+} else {
+    bark = "woof woof";
+}
+let speak = dog.name + " says " + bark + " when he wants to " + dog.activity;
+console.log(speak);
+
+let taxi = {
+    make: "Webville Motors",
+    model: "Taxi",
+    year: 1955,
+    color: "yellow",
+    passengers: 4,
+    convertible: false,
+    mileage: 281341
+};
+
+function prequal(car) {
+    if (car.mileage > 10000) {
+        return false;
+    } else if (car.year > 1960) {
+        return false;
+    }
+    return true;
 }
 
-//change a aproperty
-fiat.mileage = 10000;
-
-//how to add a new property
-fiat.needsWashing = true;
-
-//how to compute with properties
-if (fiat.year > 1965) {
-    classic = true;
+let worthALook1 = prequal(taxi);
+if (worthALook1) {
+    console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+    console.log("You should really pass on the " + taxi.make + " " + taxi.model);
 }
-for (let i = 0; i < fiat.passengers; i++) {
-    addPersonToCar();
+let worthALook2 = prequal(cadi);
+if (worthALook2) {
+    console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+    console.log("You should really pass on the " + taxi.make + " " + taxi.model);
+}
+let worthALook3 = prequal(fiat);
+if (worthALook3) {
+    console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+    console.log("You should really pass on the " + taxi.make + " " + taxi.model);
+}
+let worthALook4 = prequal(chevy);
+if (worthALook4) {
+    console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+    console.log("You should really pass on the " + taxi.make + " " + taxi.model);
 }
