de59c6790566e84b58b7bfae544e0a542e6cde73
hima-del <himajaharidas77@gmail.com>
2020-08-01 10:03:08 +0530

    basics of objects

--- headfirst_javascript/chapter5.js
+++ headfirst_javascript/chapter5.js
@@ -8,3 +8,44 @@ let chevy = {
     convertible: false,
     mileage: 1021
 };
+
+//another object-exercise
+let cadi = {
+    make: "cadi",
+    model: "GM Cadillac",
+    year: 1955,
+    color: "tan",
+    convertible: false,
+    mileage: 12892,
+    passengers: 5
+}
+
+//how object properties work
+let fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000
+};
+
+let miles = fiat.mileage;
+if (miles < 2000) {
+    buyIt();
+}
+
+//change a aproperty
+fiat.mileage = 10000;
+
+//how to add a new property
+fiat.needsWashing = true;
+
+//how to compute with properties
+if (fiat.year > 1965) {
+    classic = true;
+}
+for (let i = 0; i < fiat.passengers; i++) {
+    addPersonToCar();
+}
