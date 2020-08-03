fe14f57212c2a2747238af2c86a7d688eaff29b7
hima-del <himajaharidas77@gmail.com>
2020-08-03 21:50:44 +0530

    chapter 7 type coversions

--- headfirst_javascript/chapter7.js
+++ headfirst_javascript/chapter7.js
@@ -45,3 +45,15 @@ if (isNaN(test12)) {
 } else {
     console.log("no");
 }
+
+if (isNaN(test1)) {
+    console.log("yes");
+} else {
+    console.log("no");
+}
+
+if (99 == "99") {
+    console.log("A number equals a string!");
+} else {
+    console.log("No way a number equals a string");
+}
