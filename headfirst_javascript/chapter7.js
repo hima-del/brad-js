5e9a308d4fc18f9641e935cdc385b8d015db261d
hima-del <himajaharidas77@gmail.com>
2020-08-03 08:29:34 +0530

    chapter 7 grabing dom elements

--- headfirst_javascript/chapter7.js
+++ headfirst_javascript/chapter7.js
@@ -1,4 +1,19 @@
 let access = document.getElementById("code9");
 let code = access.innerHTML;
 code = code + " midnight";
-alert(code);
+console.log(code);
+
+let planets = document.getElementById("greenplanets");
+planets.innerHTML = "Red Alert: hit by phaser fire!";
+
+function addSongs() {
+    let song1 = document.getElementById('song1');
+    let song2 = document.getElementById('song2');
+    let song3 = document.getElementById('song3');
+
+    song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
+    song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
+    song3.innerHTML = "I Code the Line, by Johnny JavaScript";
+}
+
+window.onload = addSongs;
