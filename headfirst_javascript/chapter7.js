b63dc028e6c99b4e437dcfab4fa16828e087b6de
hima-del <himajaharidas77@gmail.com>
2020-08-04 12:08:15 +0530

    chapter 7 truthy falsy

--- headfirst_javascript/chapter7.js
+++ headfirst_javascript/chapter7.js
@@ -57,3 +57,82 @@ if (99 == "99") {
 } else {
     console.log("No way a number equals a string");
 }
+
+
+let chevy = {
+    make: "Chevy",
+    model: "Bel Air"
+};
+let taxi = {
+    make: "Webville Motors",
+    model: "Taxi"
+};
+let fiat1 = {
+    make: "Fiat",
+    model: "500"
+};
+let fiat2 = {
+    make: "Fiat",
+    model: "500"
+};
+let lot = [chevy, taxi, fiat1, fiat2];
+
+function findCarInLot(car) {
+    for (let i = 0; i < lot.length; i++) {
+        if (car === lot[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+let loc1 = findCarInLot(fiat2);
+let loc2 = findCarInLot(taxi);
+let loc3 = findCarInLot(chevy);
+let loc4 = findCarInLot(fiat1);
+
+console.log(loc1);
+console.log(loc2);
+console.log(loc3);
+console.log(loc4);
+
+
+function lieDetectorTest() {
+    let lies = 0;
+    let stolenDiamond = {};
+    if (stolenDiamond) {
+        console.log("You stole the diamond");
+        lies++;
+    }
+    let car = {
+        keysInPocket: null
+    };
+    if (car.keysInPocket) {
+        console.log("Uh oh, guess you stole the car!");
+        lies++;
+    }
+    if (car.emptyGasTank) {
+        console.log("You drove the car after you stole it!");
+        lies++;
+    }
+    let foundYouAtTheCrimeScene = [];
+    if (foundYouAtTheCrimeScene) {
+        console.log("A sure sign of guilt");
+        lies++;
+    }
+    if (foundYouAtTheCrimeScene[0]) {
+        console.log("Caught with a stolen item!");
+        lies++;
+    }
+    let yourName = " ";
+    if (yourName) {
+        console.log("Guess you lied about your name");
+        lies++;
+    }
+    return lies;
+}
+let numberOfLies = lieDetectorTest();
+console.log("You told " + numberOfLies + " lies!");
+if (numberOfLies >= 3) {
+    console.log("Guilty as charged");
+}
