9957b2395e6c097c77fa646ab3ffde9ef08afb73
hima-del <himajaharidas77@gmail.com>
2020-08-08 15:14:34 +0530

    chapter 9 unblurring images on clicking it

--- headfirst_javascript/chapter9.js
+++ headfirst_javascript/chapter9.js
@@ -1,4 +1,36 @@
-function pageLoadHandler() {
-    alert("I am live");
+// function pageLoadHandler() {
+//     alert("I am live");
+// }
+// window.onload = pageLoadHandler;
+
+// window.onload = init;
+
+// function init() {
+//     let image = document.getElementById("zero");
+//     image.onclick = showAnswer;
+// }
+
+// function showAnswer() {
+//     let image = document.getElementById("zero");
+//     image.src = "img/zero.jpg";
+// }
+window.onload = init;
+
+function init() {
+    let images = document.getElementsByTagName("img");
+    //console.log(images);
+    for (let i = 0; i < images.length; i++) {
+        // console.log(images[i]);
+        images[i].onclick = showAnswer;
+    }
 }
-window.onload = pageLoadHandler;
+
+function showAnswer(eventObj) {
+    let image = eventObj.target;
+    //console.log(image);
+    let name = image.id;
+    //console.log(name);
+    name = name + ".jpg"
+        //console.log(name);
+    image.src = `img/${name}`;
+}
