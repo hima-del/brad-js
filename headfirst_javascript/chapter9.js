d847966e0e89ce8d3b76ce92d43654bbff20a865
hima-del <himajaharidas77@gmail.com>
2020-08-08 21:49:36 +0530

    chapter 9 completed

--- headfirst_javascript/chapter9.js
+++ headfirst_javascript/chapter9.js
@@ -1,3 +1,30 @@
+//deblurring omage on mouseover event
+
+window.onload = init;
+
+function init() {
+    let images = document.getElementsByTagName("img");
+    for (let i = 0; i < images.length; i++) {
+        images[i].onmouseover = showAnswer;
+        images[i].onmouseout = reblur;
+    }
+}
+
+function showAnswer(e) {
+    let image = e.target;
+    let name = image.id;
+    name = name + ".jpg";
+    image.src = `img/${name}`
+}
+
+function reblur(e) {
+    let image = e.target;
+    let name = image.id;
+    name = name + "blur.jpg"
+    image.src = `img/${name}`
+}
+
+
 // function pageLoadHandler() {
 //     alert("I am live");
 // }
@@ -14,7 +41,7 @@
 //     let image = document.getElementById("zero");
 //     image.src = "img/zero.jpg";
 // }
-window.onload = init;
+// window.onload = init;
 
 // function init() {
 //     let images = document.getElementsByTagName("img");
@@ -33,16 +60,43 @@ window.onload = init;
 //     name = name + ".jpg"
 //         //console.log(name);
 //     image.src = `img/${name}`;
+//     setTimeout(reblur, 2000, image);
 // }
 
-function init() {
-    let map = document.getElementById("map");
-    map.onmousemove = showCoords;
-}
+// function reblur(image) {
+//     let name = image.id;
+//     name = name + "blur.jpg";
+//     image.src = `img/${name}`
+// }
+// function init() {
+//     let map = document.getElementById("map");
+//     map.onmousemove = showCoords;
+// }
 
-function showCoords(eventObj) {
-    let map = document.getElementById("coords");
-    let x = eventObj.clientX;
-    let y = eventObj.clientY;
-    map.innerHTML = "map coordinates: " + x + ", " + y;
-}
+// function showCoords(eventObj) {
+//     let map = document.getElementById("coords");
+//     let x = eventObj.clientX;
+//     let y = eventObj.clientY;
+//     map.innerHTML = "map coordinates: " + x + ", " + y;
+// }
+//time out function
+// function timeHandler() {
+//     alert("hey ... what are you doing there");
+// }
+
+// setTimeout(timeHandler, 5000);
+
+//setTimeInterval
+
+// let tick = true;
+
+// function ticker() {
+//     if (tick) {
+//         console.log("tick");
+//         tick = false;
+//     } else {
+//         console.log("tock");
+//         tick = true;
+//     }
+// }
+// setInterval(ticker, 1000);
