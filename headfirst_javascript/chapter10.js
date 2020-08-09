e4b5680ec914b8993114d0dc7d2ebf86fca1e050
hima-del <himajaharidas77@gmail.com>
2020-08-09 19:20:35 +0530

    chapter10 initial commit

---
+++ headfirst_javascript/chapter10.js
@@ -0,0 +1,15 @@
+function quack(num) {
+    for (let i = 0; i < num; i++) {
+        console.log("quack");
+    }
+}
+
+quack(3);
+
+let fly = function(num) {
+    for (let i = 0; i < num; i++) {
+        console.log("flying");
+    }
+};
+
+fly(3);
