0b7bc1be98693d1ac92cad36849e60f9df6626d9
hima-del <himajaharidas77@gmail.com>
2020-08-10 16:12:30 +0530

    passing functions to functions exercise completed

--- headfirst_javascript/chapter10.js
+++ headfirst_javascript/chapter10.js
@@ -98,3 +98,24 @@ processPassengers(passengers, printPassenger);
 //     if (word === "Goodbye") return "Aloha";
 // }
 // sayIt(hawaiianTranslator);
+
+function fun(echo) {
+    console.log(echo);
+};
+
+fun("hello");
+
+function boo(aFunction) {
+    aFunction("boo");
+}
+boo(fun);
+console.log(fun);
+fun(boo);
+let moreFun = fun;
+moreFun("hello again");
+
+function echoMaker() {
+    return fun;
+}
+let bigFun = echoMaker();
+bigFun("Is there an echo?");
