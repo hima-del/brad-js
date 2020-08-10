92f59f8d1586e87dd7f11e0fbb791cc1c00cf232
hima-del <himajaharidas77@gmail.com>
2020-08-10 14:20:48 +0530

    functions as values

--- headfirst_javascript/chapter10.js
+++ headfirst_javascript/chapter10.js
@@ -1,3 +1,5 @@
+//function declaration
+
 function quack(num) {
     for (let i = 0; i < num; i++) {
         console.log("quack");
@@ -6,6 +8,8 @@ function quack(num) {
 
 quack(3);
 
+//function expression
+
 let fly = function(num) {
     for (let i = 0; i < num; i++) {
         console.log("flying");
@@ -13,3 +17,25 @@ let fly = function(num) {
 };
 
 fly(3);
+
+//sharpen your pencil exercise
+
+let winner = function() { console.log("WINNER!") };
+let loser = function() { console.log("LOSER!") };
+
+winner();
+
+let a = winner;
+let b = loser;
+let c = loser;
+a();
+b();
+
+c = a;
+a = b;
+b = c;
+c = a;
+a = c;
+a = b;
+b = c;
+a();
