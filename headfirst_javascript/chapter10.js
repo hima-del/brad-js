d8034d12eee2d145388b454ecf566a893ccf6eea
hima-del <himajaharidas77@gmail.com>
2020-08-10 15:24:08 +0530

    passing functions to functions

--- headfirst_javascript/chapter10.js
+++ headfirst_javascript/chapter10.js
@@ -39,3 +39,62 @@ a = c;
 a = b;
 b = c;
 a();
+
+//first class functions
+
+let passengers = [{ name: "Jane Doloop", paid: true },
+    { name: "Dr.Evel", paid: true },
+    { name: "Sue Property", paid: false },
+    { name: "John Funcall", paid: true }
+];
+
+function checkNoFlyList(passenger) {
+    return (passenger.name === "Dr.Evel");
+}
+
+function checkNotPaid(passenger) {
+    return (!passenger.paid)
+}
+
+function printPassenger(passenger) {
+    let message = passenger.name;
+    if (passenger.paid === true) {
+        console.log(message + " has paid");
+    } else if (passenger.paid === false) {
+        console.log(message + " not paid");
+    }
+}
+
+
+function processPassengers(passengers, testFunction) {
+    for (let i = 0; i < passengers.length; i++) {
+        if (testFunction(passengers[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let allCanFly = processPassengers(passengers, checkNoFlyList);
+if (!allCanFly) {
+    console.log("The plane can't take off: we have a passenger on the no fly list ");
+}
+
+let allPaid = processPassengers(passengers, checkNotPaid);
+if (!allPaid) {
+    console.log("the plane can't take off: not everyone has paid");
+}
+
+processPassengers(passengers, printPassenger);
+//sharpen your pencil exercise
+
+// function sayIt(translator) {
+//     let phrase = translator("Hello");
+//     console.log(phrase);
+// }
+
+// function hawaiianTranslator(word) {
+//     if (word === "Hello") return "Aloha";
+//     if (word === "Goodbye") return "Aloha";
+// }
+// sayIt(hawaiianTranslator);
